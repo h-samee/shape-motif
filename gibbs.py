d49320846bd1e720a956e47f256e42756624be1f
Hassan Samee <hassansamee@gladstoinstall5.gateway.pace.com>
2016-01-12 02:59:40 -0800

    gibbs.py after: changing gibbs sampling convergence criteria, test for several values of the count of std dev, and modifications to output all occurrences of the motif.

--- gibbs.py
+++ gibbs.py
@@ -22,8 +22,12 @@ def distance(x, y):
 def gibbs_motif_finder(shape_data, window_size):
 	n_seq = len(shape_data)
 	seq_len = len(shape_data[0])
+	##parameters specific to this function
+	max_consecutive_iters_wo_improvement = 10
+	n_consecutive_iters_wo_improvement = 0
+	epsilon_factor_improvement = 1e-5
 
-	#generate initial window locations
+	##generate initial window locations
 	window_locs = []
 	for i in range(n_seq):
 		window_locs.append(random.randint(0, seq_len - window_size))
@@ -33,6 +37,7 @@ def gibbs_motif_finder(shape_data, window_size):
 
 	while True:
 		cur_window_locs = window_locs[:]
+		#find the current best candidate for each sequence
 		for i in range(n_seq):
 			cur_shape_data = shape_data[i]
 			cur_best_distance = float("inf")
@@ -46,22 +51,31 @@ def gibbs_motif_finder(shape_data, window_size):
 				if cur_distance < cur_best_distance:
 					cur_best_distance = cur_distance
 					cur_window_locs[i] = j
+		#compute the current all-pair-distance -- how coherent/homogeneous are the shape features
 		cur_all_pair_distance = 0
 		for i in range(n_seq):
 			for j in range(i + 1, n_seq):
 				window_i = shape_data[i][cur_window_locs[i] : cur_window_locs[i] + window_size]
 				window_j = shape_data[j][cur_window_locs[j] : cur_window_locs[j] + window_size]
 				cur_all_pair_distance += distance(window_i, window_j)
-		if cur_all_pair_distance < all_pair_distance:
+		
+		#update and continue, or break and terminate
+		if not (cur_all_pair_distance < all_pair_distance):
+			break
+		elif cur_all_pair_distance > all_pair_distance * (1 - epsilon_factor_improvement):
 			all_pair_distance = cur_all_pair_distance
 			window_locs = cur_window_locs[:]
+			n_consecutive_iters_wo_improvement += 1
+			if n_consecutive_iters_wo_improvement == max_consecutive_iters_wo_improvement:
+				break
 		else:
-			break
+			all_pair_distance = cur_all_pair_distance
+			window_locs = cur_window_locs[:]
+			n_consecutive_iters_wo_improvement = 0
 
 	return window_locs
-	#random.jumpahead(n_seq)
 
-def compute_ranges(shape_data, motif_window_locs, window_size):
+def compute_ranges(shape_data, motif_window_locs, window_size, sigma_count):
 	n_seq = len(shape_data)
 	ranges = []
 	
@@ -75,8 +89,8 @@ def compute_ranges(shape_data, motif_window_locs, window_size):
 		mean_at_i = np.mean(np.array(vals_at_i))
 		std_at_i = np.std(np.array(vals_at_i))
 
-		min_val = mean_at_i - 2 * std_at_i 
-		max_val = mean_at_i + 2 * std_at_i 
+		min_val = mean_at_i - sigma_count * std_at_i 
+		max_val = mean_at_i + sigma_count * std_at_i 
 		
 		#min_val = np.min(np.array(vals_at_i))
 		#max_val = np.max(np.array(vals_at_i))
@@ -98,30 +112,39 @@ def does_motif_match_window(motif_as_range, window):
 
 	return True 
 
-def does_seq_contain_motif(cur_shape_data, motif_as_range):
+def list_motif_occurrences(cur_shape_data, motif_as_range):
 	seq_len = len(cur_shape_data)
 	window_size = len(motif_as_range)
-	
+	occurrence_list = []
 	for i in range(seq_len - window_size + 1):
 		cur_window = cur_shape_data[i : i + window_size]
 		if does_motif_match_window(motif_as_range, cur_window):
-			return True
+			occurrence_list.append(i)
 	
-	return False
+	return occurrence_list
 
 def count_occurrences(motif_as_range, shape_data):
+"""
+Note: 
+-- This function returns:
+---- number of sequences that contain the motif and
+---- locations of the motif's occurrences (overlapping occurrences allowed) in each sequence (as a dictionary)
+"""
 	n_seq = len(shape_data)
 	seq_len = len(shape_data[0])
 	window_size = len(motif_as_range)
 	
 	occurrence_count = 0
-	
+	occurrence_dict = {}
+
 	for i in range(n_seq):
 		cur_shape_data = shape_data[i]
-		if does_seq_contain_motif(cur_shape_data, motif_as_range):
+		lst = list_motif_occurrences(cur_shape_data, motif_as_range)
+		if len(lst) > 0:
 			occurrence_count += 1
+		occurrence_dict[i] = lst
 
-	return occurrence_count
+	return occurrence_count, occurrence_dict
 
 def read_shape_data(file_name):
 	bed_lines = []
@@ -130,7 +153,7 @@ def read_shape_data(file_name):
 		for line in f:
 			vals = line.split()
 			if len(vals) != 8:
-				print('Possible error: some fields missing in data file intersecting .bw and Leslie data')
+				print('Possible error: some fields missing in data file integrating .bw and Leslie data')
 			else:
 				chromosome = vals[0]
 				start_coord = int(vals[1])
@@ -146,15 +169,20 @@ def read_shape_data(file_name):
 	return shape_data, bed_lines
 """
 Notes:
-1. We assume there is no header line in the shape data or in the bed files
-2. in the function count_occurrences, we allow overlapping occurrences
+-- This version takes inputs for the Arvey et al. paper (Leslie Lab).
+---- Training (chip and ctrl peaks) and test (training and ctrl peaks)
+---- window size
+---- Number of motifs to find
+---- output directory
+-- We assume there is no header line in the shape data or in the bed files
+-- in the function count_occurrences, we allow overlapping occurrences
 """
 
 def main(argv = None):
 	if argv is None:
 		argv = sys.argv
 	
-	#input args
+	##input args
 	chip_data_file_name = argv[1] 
 	ctrl_data_file_name = argv[2] 
 	window_size = int(argv[3]) #length of motif
@@ -163,101 +191,111 @@ def main(argv = None):
 	test_ctrl_data_file_name = argv[6]
 	output_dir = argv[7]
 
-	#####
-	#read shape data
+	##read shape data and bed file
+	#chip peaks (train)
 	chip_shape_data, chip_bed_lines = read_shape_data(chip_data_file_name)
 	n_chip_seq = len(chip_shape_data)
-
-	#read control shape data
+	#control peaks (train)
 	ctrl_shape_data, ctrl_bed_lines = read_shape_data(ctrl_data_file_name)
 	n_ctrl_seq = len(ctrl_shape_data)
-
-	#####
-	#read test shape data
+	#chip peaks (test)
 	test_chip_shape_data, test_chip_bed_lines = read_shape_data(test_chip_data_file_name)
 	n_test_chip_seq = len(test_chip_shape_data)
-
-	#read test control shape data
+	#control peaks (train)
 	test_ctrl_shape_data, test_ctrl_bed_lines = read_shape_data(test_ctrl_data_file_name)
 	n_test_ctrl_seq = len(test_ctrl_shape_data)
 
+	##set output files
 	file_name_prefix = os.path.split(chip_data_file_name)[1].split('.')[0]
+	#output file names	
 	summary_file_name = output_dir + "/" + file_name_prefix + ".summary"
 	motif_shape_file_name = output_dir + "/" + file_name_prefix + ".shape"
-	range_motif_file_name = output_dir + "/" + file_name_prefix + ".range_motif"
 	motif_bed_file_name = output_dir + "/" + file_name_prefix + ".bed"
-
+	#open output files
 	summary_file = open(summary_file_name, "w")
 	motif_shape_file = open(motif_shape_file_name, "w")
-	range_motif_file = open(range_motif_file_name, "w")
 	motif_bed_file = open(motif_bed_file_name, "w")
 	
+	##initiate the random number generator based on the name of the output directory.
+	##the output directory name contains cell type, TF name, shape feature, and length.
+	random.seed(output_dir)
+
 	for iter_count in range(max_iter):
-		random.jumpahead(n_chip_seq * (1 + iter_count))
-		#compute motif
+		##compute motif
 		motif_window_locs = gibbs_motif_finder(chip_shape_data, window_size)
 		
-		#compute statistical significance
-		motif_as_range = compute_ranges(chip_shape_data, motif_window_locs, window_size)
+		for sigma_count in [0.5, 1, 1.5, 2]:
+			##compute statistical significance
+			motif_as_range = compute_ranges(chip_shape_data, motif_window_locs, window_size, sigma_count)
 
-		#####
-		n_chip_seq_with_shape = count_occurrences(motif_as_range, chip_shape_data)
-		n_ctrl_seq_with_shape = count_occurrences(motif_as_range, ctrl_shape_data)
+			#significance for training set
+			n_chip_seq_with_shape, chip_occurrences = count_occurrences(motif_as_range, chip_shape_data)
+			n_ctrl_seq_with_shape, ctrl_occurrences = count_occurrences(motif_as_range, ctrl_shape_data)
 
-		total_seq = n_chip_seq + n_ctrl_seq
-		n_seq_with_shape = n_chip_seq_with_shape + n_ctrl_seq_with_shape
-		neg_log_p_val = -math.log10(stats.hypergeom.sf(n_chip_seq_with_shape - 1, total_seq, n_chip_seq, n_seq_with_shape))
-		
-		#####
-		n_test_chip_seq_with_shape = count_occurrences(motif_as_range, test_chip_shape_data)
-		n_test_ctrl_seq_with_shape = count_occurrences(motif_as_range, test_ctrl_shape_data)
+			total_seq = n_chip_seq + n_ctrl_seq
+			n_seq_with_shape = n_chip_seq_with_shape + n_ctrl_seq_with_shape
+			neg_log_p_val = -math.log10(stats.hypergeom.sf(n_chip_seq_with_shape - 1, total_seq, n_chip_seq, n_seq_with_shape))
+			
+			#significance for test set
+			n_test_chip_seq_with_shape, test_chip_occurrences = count_occurrences(motif_as_range, test_chip_shape_data)
+			n_test_ctrl_seq_with_shape, test_ctrl_occurrences = count_occurrences(motif_as_range, test_ctrl_shape_data)
 
-		total_test_seq = n_test_chip_seq + n_test_ctrl_seq
-		n_test_seq_with_shape = n_test_chip_seq_with_shape + n_test_ctrl_seq_with_shape
-		neg_log_p_val_test = -math.log10(stats.hypergeom.sf(n_test_chip_seq_with_shape - 1, total_test_seq, n_test_chip_seq, n_test_seq_with_shape))
+			total_test_seq = n_test_chip_seq + n_test_ctrl_seq
+			n_test_seq_with_shape = n_test_chip_seq_with_shape + n_test_ctrl_seq_with_shape
+			neg_log_p_val_test = -math.log10(stats.hypergeom.sf(n_test_chip_seq_with_shape - 1, total_test_seq, n_test_chip_seq, n_test_seq_with_shape))
 
-		#generate outputs
-		#summary: p-values
-		summary_file.write("motif: %d\tneg_log_p_val: %f\tn_chip_w_shp: %d\tn_ctrl_w_shp: %d" %\
-		 (iter_count + 1, neg_log_p_val, n_chip_seq_with_shape, n_ctrl_seq_with_shape))
+			##generate outputs
+			#summary: p-values
+			summary_file.write("motif:%d;sigma:%f;neg_log_p_val:%f;n_chip_w_shp:%d;n_ctrl_w_shp:%d" %\
+			 (iter_count + 1, sigma_count, neg_log_p_val, n_chip_seq_with_shape, n_ctrl_seq_with_shape))
 
-		summary_file.write("\tneg_log_p_val_test: %f\tn_test_chip_w_shp: %d\tn_test_ctrl_w_shp: %d\n" %\
-		 (neg_log_p_val_test, n_test_chip_seq_with_shape, n_test_ctrl_seq_with_shape))
+			summary_file.write(";neg_log_p_val_test:%f;n_test_chip_w_shp:%d;n_test_ctrl_w_shp:%d\n" %\
+			 (neg_log_p_val_test, n_test_chip_seq_with_shape, n_test_ctrl_seq_with_shape))
 
-		#shapes at motif occurrences
-		#NOTE: this list may not contain a window from every sequence 
-		motif_shape_file.write("#%d\n" % (iter_count + 1))
-		for i in range(n_chip_seq):
-			cur_window = chip_shape_data[i][motif_window_locs[i] : motif_window_locs[i] + window_size]
-			if does_motif_match_window(motif_as_range, cur_window):
-				for j in range(window_size):
-					motif_shape_file.write("%f " % (chip_shape_data[i][motif_window_locs[i] + j]))
-				motif_shape_file.write("\n")
-		
-		#range motif
-		range_motif_file.write("#%d\n" % (iter_count + 1))
-		for i in range(window_size):
-			range_motif_file.write("%f " % (motif_as_range[i][0]))
-		range_motif_file.write("\n")
-		for i in range(window_size):
-			range_motif_file.write("%f " % (motif_as_range[i][1]))
-		range_motif_file.write("\n")
+			##shapes at and bed coordinates of motif occurrences
+			#NOTE: this list may not contain a window from every sequence 
+			motif_shape_file.write("#%d;%f\n" % (iter_count + 1, sigma_count))
+			motif_bed_file.write("#%d;%f\n" % (iter_count + 1, sigma_count))
 
-		#bed coordiates at motif occurrences
-		#NOTE: this list may not contain a line for every sequence 
-		motif_bed_file.write("#%d\n" % (iter_count + 1))
-		for i in range(n_chip_seq):
-			cur_window = chip_shape_data[i][motif_window_locs[i] : motif_window_locs[i] + window_size]
-			if does_motif_match_window(motif_as_range, cur_window):
+			for i in range(n_chip_seq):
+				lst = chip_occurrences[i]
 				[chromosome, start_coord, end_coord] = chip_bed_lines[i]
-				motif_start_coord = start_coord + motif_window_locs[i]
-				motif_end_coord = motif_start_coord + window_size
-				motif_bed_file.write("%s\t%d\t%d\n" % (chromosome, motif_start_coord, motif_end_coord))
+				for j in lst:
+					cur_window = chip_shape_data[i][j : j + window_size]
+					for k in range(window_size):
+						motif_shape_file.write("%f " % (chip_shape_data[i][j + k]))
+					motif_shape_file.write("\n")
+
+					motif_start_coord = start_coord + j
+					motif_end_coord = motif_start_coord + window_size
+					motif_bed_file.write("%s\t%d\t%d\n" % (chromosome, motif_start_coord, motif_end_coord))
+
+			###################################################################################################		
+			##shapes at motif occurrences
+			#NOTE: this list may not contain a window from every sequence 
+			motif_shape_file.write("#%d;%f\n" % (iter_count + 1, sigma_count))
+			for i in range(n_chip_seq):
+				cur_window = chip_shape_data[i][motif_window_locs[i] : motif_window_locs[i] + window_size]
+				if does_motif_match_window(motif_as_range, cur_window):
+					for j in range(window_size):
+						motif_shape_file.write("%f " % (chip_shape_data[i][motif_window_locs[i] + j]))
+					motif_shape_file.write("\n")
+
+			#bed coordiates at motif occurrences
+			#NOTE: this list may not contain a line for every sequence 
+			motif_bed_file.write("#%d;%f\n" % (iter_count + 1, sigma_count))
+			for i in range(n_chip_seq):
+				cur_window = chip_shape_data[i][motif_window_locs[i] : motif_window_locs[i] + window_size]
+				if does_motif_match_window(motif_as_range, cur_window):
+					[chromosome, start_coord, end_coord] = chip_bed_lines[i]
+					motif_start_coord = start_coord + motif_window_locs[i]
+					motif_end_coord = motif_start_coord + window_size
+					motif_bed_file.write("%s\t%d\t%d\n" % (chromosome, motif_start_coord, motif_end_coord))
 	
+	##close output files
 	summary_file.close()
 	motif_bed_file.close()
 	motif_shape_file.close()
-	range_motif_file.close()
 	return 0
 
 if __name__ == "__main__":
